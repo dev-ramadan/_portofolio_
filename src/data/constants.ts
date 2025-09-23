661b65e720031c2ec403a758af3b8cbdd2d46f05
dev-ramadan <ganna123adam@gmail.com>
2025-09-23 20:53:05 +0300

    test

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -9,9 +9,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/dev-ramadan",
-  resume:
-    "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
-  linkedin: "https://www.linkedin.com/in/ramadan-mohamed-828120330/",
+  linkedin: "https://www.linkedin.com/in/ramadan-mohamed-dev/",
   facebook: "https://www.facebook.com/rmdan.mhmd.497928",
 };
 

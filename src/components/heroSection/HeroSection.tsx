779c150bd6eb716cf0192f03624f8ed651178f25
dev-ramadan <ganna123adam@gmail.com>
2025-12-13 14:12:35 +0200

    add project

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -245,7 +245,7 @@ const HeroSection = () => {
                   
                 </div>
               </div>
-              <ResumemButton href="https://drive.google.com/file/d/1WOnT3yOzpymSe3w1SsmqLXtnBOXtwZmi/view?usp=sharing" target="_blank">Check Resume</ResumemButton>
+              <ResumemButton href="https://drive.google.com/file/d/1sbfyCrko9CgYYnM2sWqa1AdJexBBQvtt/view?usp=drive_link" target="_blank">Check Resume</ResumemButton>
             </HeroLeftContainer>
 
             <HeroRightContainer>
